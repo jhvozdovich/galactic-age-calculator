df8ad07ecb5ea46fdf732734fe2673558700958b
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-04-17 15:31:11 -0700

    add styling and navigation

--- src/main.js
+++ src/main.js
@@ -7,12 +7,15 @@ import { User } from "./../src/galactic-age.js";
 $(document).ready(function() {
   $("#birthday-form").submit(function(event) {
     event.preventDefault();
+
+    $(".age-questions").hide();
+
     let name = $("#name-input").val();
     let birthday = $("#birthday-input").val();
     let year = parseInt(birthday.slice(0, 4));
     let month = parseInt(birthday.slice(5,7)) - 1;
     let day = parseInt(birthday.slice(8));
-    
+
     let user = new User(name, year, month, day);
     let earthAge = user.findAge();
     $("#user-name").text(name);
@@ -21,14 +24,19 @@ $(document).ready(function() {
     $("#venus-years").text(user.findVenus(earthAge));
     $("#mars-years").text(user.findMars(earthAge));
     $("#jupiter-years").text(user.findJupiter(earthAge));
+
     $(".age-results").show();
     $(".big-question").show();
 
-    $(".big-question").click(function() {
-      $(".age-questions").hide();
+    $("button.big-question").click(function() {
+      $(".age-results").hide();
       $(".big-question").hide();
-      $(".life-questions").show();
-    });  
+      $(".life-form").show();
+    });
+
+    $("button.phone-home").click(function() {
+      window.location.reload();
+    });
 
     $("#life-expectancy-form").submit(function(event) {
       event.preventDefault();
@@ -39,6 +47,9 @@ $(document).ready(function() {
       user.stress = $("#stress-input").val();
 
       $(".life-expectancy-results").text(user.lifeExpectancyMessage());
+
+      $(".life-form").hide();
+      $(".final-answer").show();
     });
   });
 });
