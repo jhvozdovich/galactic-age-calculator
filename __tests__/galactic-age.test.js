c5e533d3a0a8afb11a175e7436a2e9586d7d48e3
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-04-17 09:46:40 -0700

    refactor previous User test to accept zero indexed numeric months instead of strings

--- __tests__/galactic-age.test.js
+++ __tests__/galactic-age.test.js
@@ -3,12 +3,12 @@ import { User } from "./../src/galactic-age.js";
 describe ("User", () => {
   let emma;
   beforeEach(() => {
-    emma = new User("Emma", 1995, "February", 25);
+    emma = new User("Emma", 1995, 1, 25);
   });
   test("should correctly create a user object with a name and birthday with a month, day, and year", () => {
     expect(emma.name).toEqual("Emma");
     expect(emma.year).toEqual(1995);
-    expect(emma.month).toEqual("February");
+    expect(emma.month).toEqual(1);
     expect(emma.day).toEqual(25);
   });
 
