9ffcc9f36c7a211131f268fd9752966f925690d0
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-04-17 13:05:51 -0700

    add initial UI logic and form fields

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-// import $ from "jquery";
+import $ from "jquery";
+import { User } from "./../src/galactic-age.js";
+
+$(document).ready(function() {
+  $("#birthday-form").submit(function(event) {
+    event.preventDefault();
+    let name = $("#name-input").val();
+    let birthday = $("#birthday-input").val();
+    let year = parseInt(birthday.slice(0, 4));
+    let month = parseInt(birthday.slice(5,7)) - 1;
+    let day = parseInt(birthday.slice(8));
+    
+    let user = new User(name, year, month, day);
+
+    console.log(user);
+  
+  });
+});
