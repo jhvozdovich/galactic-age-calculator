5d69e565d4172fab6d40afdea668f9b1e2a08890
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-04-17 10:14:49 -0700

    add function that passes findAge test

--- src/galactic-age.js
+++ src/galactic-age.js
@@ -7,6 +7,10 @@ export class User {
   }
 
   findAge() {
-    
+    let dateObject = new Date(this.year, this.month, this.day);
+    let subtractNow = Date.now() - dateObject;
+    let subtractNowObject = new Date(subtractNow);
+    let subtractEpoch = Math.floor(subtractNowObject.getUTCFullYear() - 1970);
+    return subtractEpoch;
   }
 }
