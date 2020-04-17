2a5f1762f7ada027e69dee4ba847839f31bde9d4
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-04-17 10:22:50 -0700

    refactor findAge to round milisecond result rather than subtract Epoch

--- src/galactic-age.js
+++ src/galactic-age.js
@@ -9,8 +9,7 @@ export class User {
   findAge() {
     let dateObject = new Date(this.year, this.month, this.day);
     let subtractNow = Date.now() - dateObject;
-    let subtractNowObject = new Date(subtractNow);
-    let subtractEpoch = Math.floor(subtractNowObject.getUTCFullYear() - 1970);
-    return subtractEpoch;
+    let roundYear = Math.floor(subtractNow / (365 * 24 * 60 * 60 * 1000));
+    return roundYear;
   }
 }
