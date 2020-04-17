2eeb24e7dae2de79a7ffa9e8032815172db87e0a
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-04-17 09:36:01 -0700

    refactor test to use beforeEach

--- __tests__/galactic-age.test.js
+++ __tests__/galactic-age.test.js
@@ -1,8 +1,11 @@
 import { User } from "./../src/galactic-age.js";
 
 describe ("User", () => {
+  let emma;
+  beforeEach(() => {
+    emma = new User("Emma", 1995, "February", 25);
+  })
   test("should correctly create a user object with a name and birthday with a month, day, and year", () => {
-    var emma = new User("Emma", 1995, "February", 25);
     expect(emma.name).toEqual("Emma");
     expect(emma.year).toEqual(1995);
     expect(emma.month).toEqual("February");
